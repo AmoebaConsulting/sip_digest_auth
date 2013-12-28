299351b403dcfbd1eb24049370f9a3fc911a982b
Daniel Jabbour <daniel@amoe.ba>
2013-12-28 00:22:26 -0500

    Adding basic testing framework

---
+++ test/test_sip_digest_auth.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'sip_digest_auth'
+
+class TestSipDigestAuth < Minitest::Test
+  def setup
+    # Someday we may have some setup
+  end
+
+  def test_nothin
+    assert_equal true, true
+  end
+
+end
